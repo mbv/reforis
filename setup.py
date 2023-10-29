50152235274e4da460e0ee2a71cc0aa3cb09d823
Konstantin Terekhov <terehovk@ya.ru>
2023-10-29 18:14:07 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class CustomBuild(build_py):
 
 setuptools.setup(
     name='reforis',
-    version='1.5.0',
+    version='1.5.1',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
